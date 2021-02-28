6f55463cbcbd913e25923aff82496148ee4965b1
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-02-28 16:25:29 -0300

    Day 2

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { Router } from 'express';
+import './database';
+import { router } from '../router';
 
 const app = express();
 
-app.get('/', (request, respose) => {
-    return respose.json({ message: 'Hello world - NLW04'});
-});
-
-app.post('/', (request, response) => {
-    return response.json({message: 'Os dados foram recebidos com sucesso!'});
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
